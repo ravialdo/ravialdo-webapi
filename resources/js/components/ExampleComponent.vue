e7a84fb955fcafbdb67ad608d6dd7c89ee657ad7
Ravialdo <ravialdo@yahoo.com>
2020-01-21 00:31:12 -0800

    update script

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Test API</div>
+
+                    <div class="card-body">
+                        Contoh hasil Pemanggilan Endpoint API
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            this.getData()
+        },
+
+        methods: {
+            getData(){
+                axios.get('/testapi')
+            }
+        }
+    }
+</script>
